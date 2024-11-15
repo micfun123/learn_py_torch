9b424c64fa7b1c609ba67ba8f1e4302f8428eab9
Michael Parker <michaelrbparker@protonmail.com>
2024-11-15 11:45:30 +0000

    MNIST

--- MNIST/models.py
+++ MNIST/models.py
@@ -6,11 +6,11 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.cov1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1) #convolutional layer
-        self.relu = nn.ReLU() #activation function
+        self.cov1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
         self.cov2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)       
         self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=1) #pooling layer 
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=1)
         self.cov3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.cov4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
