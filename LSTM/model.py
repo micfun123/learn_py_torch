567279ed6dfcd2b74697e126fd09ff9ec0d9415f
Michael Parker <michaelrbparker@protonmail.com>
2024-11-28 14:22:08 +0000

    better requirment.txt

--- LSTM/model.py
+++ LSTM/model.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader , Dataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
 
+
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(LSTM, self).__init__()
@@ -15,6 +16,8 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
+        self.dropout = nn.Dropout(0.5)
+        
         
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
